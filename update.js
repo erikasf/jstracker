f39d8284dc9536ecf3b378d77e120ad0baeed298
erika <erika@erikasf.com>
2015-06-07 11:53:40 -0700

    update

---
+++ update.js
@@ -0,0 +1,25 @@
+  this.clone = function() {
+    var c = new headtrackr.camshift.Rectangle();
+    c.height = this.height;
+    c.width = this.width;
+    c.x = this.x;
+    c.y = this.y;
+    return c;
+    var collision = function (x,y){
+    	var headx = c.x;
+    	var sidey = c.y;
+    	var butxy = function getOffset(el) {
+  		el = el.getBoundingClientRect();
+  		return {
+    			left: el.left + window.scrollX,
+    				top: el.top + window.scrollY
+				 }
+			}
+    		}
+	 }
+	 if( butxy ==  headx ) || (butxy ==s idey){
+	 	$(.call).click(){
+	 		console.log("wooot it works");
+	 	}
+	 }
+};
